f810c7bf786cf460c78b5d992acd9bb8eeb09944
Rival Achmad Firdaus <159166059+RivalAchmad@users.noreply.github.com>
2025-03-12 11:15:33 +0700

    Update login_correct_test.py

--- tests/login_correct_test.py
+++ tests/login_correct_test.py
@@ -9,32 +9,32 @@ class LoginCorrectCredentialsTestCase(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
         option = webdriver.FirefoxOptions()
-        option.add_argument('--headless')
+        option.add_argument('--headless')  
         cls.browser = webdriver.Firefox(options=option)
+        cls.browser.implicitly_wait(10) 
 
     def test_1_login_correct_credentials(self):
         login_url = 'http://localhost/login.php'
         self.browser.get(login_url)
 
+        WebDriverWait(self.browser, 10).until(EC.presence_of_element_located((By.ID, 'inputUsername')))
+
         self.browser.find_element(By.ID, 'inputUsername').send_keys('admin')
         self.browser.find_element(By.ID, 'inputPassword').send_keys('nimda666!')
         self.browser.find_element(By.TAG_NAME, 'button').click()
-        
-        # Tunggu redirect dan pastikan berada di halaman index.php
-        WebDriverWait(self.browser, 10).until(EC.url_contains("index.php"))
-        self.assertIn("index.php", self.browser.current_url)
-    
+
+        WebDriverWait(self.browser, 10).until(EC.url_contains('index.php'))
+
     def test_2_index_page(self):
-        try:
-            expected_result = "Halo, "
-            
-            # Tunggu hingga elemen h2 muncul di halaman
-            element = WebDriverWait(self.browser, 10).until(
-                EC.presence_of_element_located((By.TAG_NAME, "h2"))
-            )
-            actual_result = element.text
-            self.assertIn(expected_result, actual_result)
-    
+        expected_text = "Halo, "
+
+        element = WebDriverWait(self.browser, 10).until(
+            EC.presence_of_element_located((By.XPATH, "/html/body/div[1]/h2"))
+        )
+        actual_text = element.text
+
+        self.assertIn(expected_text, actual_text)
+
     @classmethod
     def tearDownClass(cls):
         cls.browser.quit()
