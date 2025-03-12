db6ea062286746058f09b0e33442a5461b2dee26
Rival Achmad Firdaus <159166059+RivalAchmad@users.noreply.github.com>
2025-03-12 14:30:33 +0700

    Update login_correct_test.py

--- tests/login_correct_test.py
+++ tests/login_correct_test.py
@@ -1,28 +1,36 @@
-import unittest
+import unittest, os
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
-class LoginCorrectCredentialsTestCase(unittest.TestCase):
+class LoginTestCase(unittest.TestCase):
 
     @classmethod
     def setUpClass(cls):
         option = webdriver.FirefoxOptions()
         option.add_argument('--headless')
         cls.browser = webdriver.Firefox(options=option)
+        try:
+            cls.url = os.environ['URL']
+        except:
+            cls.url = "http://localhost"
 
-    def test_1_login_correct_credentials(self):
-        login_url = 'http://localhost/login.php'
+    def test(self):
+        self.login_correct_credentials()
+        self.index_page()
+
+    def login_correct_credentials(self):
+        login_url = self.url +'/login.php'
         self.browser.get(login_url)
 
         self.browser.find_element(By.ID, 'inputUsername').send_keys('admin')
         self.browser.find_element(By.ID, 'inputPassword').send_keys('nimda666!')
-        self.browser.find_element(By.TAG_NAME, 'button').click
+        self.browser.find_element(By.TAG_NAME, 'button').click()
 
-    def test_2_index_page(self):
+    def index_page(self):
         expected_result = "admin"
         actual_result = self.browser.find_element(By.XPATH, "//h2[contains(text(),'Howdy, damn')]").text.split(', ')[1]
         self.assertIn(expected_result, actual_result)
-        
+
     @classmethod
     def tearDownClass(cls):
         cls.browser.quit()
